863d164c448a216286d3076d6cc9b89daa17fe37
solcortes2018 <solecito-2109@hotmail.com>
2022-07-18 22:06:19 -0500

    subiendo formato tarjeta

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import validator from './validator.js';
 
+function products()
+document.getElementById('buttom')
+
+var item1= (".item1")
+item1.style.display ="block"
+ 
+var item2= (".item2")
+item2.style.display ="block"
+
+var item3= (".item3")
+item3.style.display ="block"
+
 console.log(validator);
