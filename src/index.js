f9b81b7e7b29e568c0ed20a50d81cf0c8fa3a160
solcortes2018 <solecito-2109@hotmail.com>
2022-07-26 23:13:18 -0500

    subiendo mask

--- src/index.js
+++ src/index.js
@@ -1,19 +1,44 @@
 import validator from './validator.js';
 
-function products()
-{  
-let productos= document.getElementById('inicio');
+//cambio de catalogo a tarjeta y viceversa//
 
-let item1= document.querySelector(".item1")
-item1.style.display ="block";
- 
-let item2= document.querySelector(".item2")
-item2.style.display ="block";
+let agregar1= document.getElementById("agregar1")
+agregar1.addEventListener( "click",cambiarVista) 
+let agregar2= document.getElementById("agregar2")
+agregar2.addEventListener( "click",cambiarVista) 
+let agregar3= document.getElementById("agregar3")
+agregar3.addEventListener( "click",cambiarVista) 
 
-let item3= document.querySelector(".item3")
-item3.style.display ="block";
+function cambiarVista() {
+    document.getElementById ("items").style.display ="none";
+    document.getElementById ("container_formulario").style.display="block";
 }
 
 
+let catalogo=document.getElementById ("#catalogo")
+ catalogo.addEventListener("click", mostrarCatalogo)
 
-console.log(validator);
+ function mostrarCatalogo(){
+    document.getElementById("container_formulario").style.display="none"
+    document.getElementById ("items").style.display="block"
+ }
+
+ //obtengo la información del campo de #de tarjeta y pongo la mascara//
+ 
+ let validar1= document.getElementById("pagar")
+ validar1.addEventListener("click", botonPagar)
+
+ function botonPagar(){
+    let creditCardNumber=document.getElementById("numero_tarjeta").value;
+ 
+    if(numero_tarjeta.value == "") {
+        alert("Ingresa el número de tarjeta de crédito");
+    }
+    else {
+        let mostrarCuatro = maskify(creditCardNumber);
+        cardNumber.value = mostrarCuatro;
+    }
+ }
+ 
+
+//console.log(validator);//
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,17 @@
-const validator = {
-  // ...
-};
+const validator = {};
 
+
+
+function maskify(creditCardNumber) {
+
+  if (creditCardNumber.length > 4) {
+    let numeroOculto = "#".repeat(creditCardNumber.length - 4);
+    let mostrar=creditCardNumber.substring=(creditCardNumber.length -4);
+    let mascara=numeroOculto + mostrar;
+    return mascara;}
+    else{
+      return creditCardNumber;
+    }
+
+}
 export default validator;
