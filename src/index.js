9fb7fbd042b0ee4a9790606345e8d7b320e3fe9b
solcortes2018 <solecito-2109@hotmail.com>
2022-08-03 11:37:43 -0500

    funcion isvalid

--- src/index.js
+++ src/index.js
@@ -1,42 +1,62 @@
-import  validator  from "./validator.js";
+import validator from "./validator.js";
 
 //cambio de catalogo a tarjeta y viceversa//
 
-let agregar1= document.getElementById("agregar1")
-agregar1.addEventListener( "click",cambiarVista) 
-let agregar2= document.getElementById("agregar2")
-agregar2.addEventListener( "click",cambiarVista) 
-let agregar3= document.getElementById("agregar3")
-agregar3.addEventListener( "click",cambiarVista) 
+let agregar1 = document.getElementById("agregar1")
+agregar1.addEventListener("click", cambiarVista)
+let agregar2 = document.getElementById("agregar2")
+agregar2.addEventListener("click", cambiarVista)
+let agregar3 = document.getElementById("agregar3")
+agregar3.addEventListener("click", cambiarVista)
 
 function cambiarVista() {
-    document.getElementById ("items").style.display ="none";
-    document.getElementById ("container_formulario").style.display="block";
+   document.getElementById("items").style.display = "none";
+   document.getElementById("container_formulario").style.display = "block";
 }
 
 
-let catalogo=document.getElementById ("#catalogo")
- catalogo.addEventListener("click", mostrarCatalogo)
+let catalogo = document.getElementById("#catalogo")
+catalogo.addEventListener("click", mostrarCatalogo)
 
- function mostrarCatalogo(){
-    document.getElementById("container_formulario").style.display="none"
-    document.getElementById ("items").style.display="block"
- }
+function mostrarCatalogo() {
+   document.getElementById("container_formulario").style.display = "none"
+   document.getElementById("items").style.display = "block"
+}
 
- //tarjeta//
- 
- const creditCardNumber = document.querySelector("#numero_tarjeta");
- const numeroPlastico = document.querySelector(".numeroT");
- console.log(creditCardNumber);
+//tarjeta//
+let esValido= document.querySelector(".mensaje")
+function validacion () {
+   let boton_pagar = creditCardNumber.value;      
+           let validation = validator.isValid(creditCardNumber.value);
+           if (validation==true) {
+               esValido.innerHTML = 'Transacción Aprobada ¡Gracias por tu compra!';
+           }
+           else {
+              esValido.innerHTML = 'Tarjeta inválida, comunicate con tu entidad bancaria';
+           }
+       }
+       
+pagar.addEventListener("click", validacion);
 
- creditCardNumber.addEventListener("keyup", function(event){
-  let mascara= validator.maskify(creditCardNumber.value);
-  console.log(mascara);
-   
+
+
+const creditCardNumber = document.querySelector("#numero_tarjeta");
+const numeroPlastico = document.querySelector(".numeroT");
+console.log(creditCardNumber);
+
+creditCardNumber.addEventListener("keyup", function (event) {
+   let mascara = validator.maskify(creditCardNumber.value);
+   numeroPlastico.textContent = mascara;
 })
 
+//nombre //
+const nombre_uno = document.querySelector("#nombre");
+const nombrePlastico = document.querySelector(".nombreT");
 
- 
- 
+nombre_uno=addEventListener("keyup", function(event) {
+   let name= event.currentTarget.value;
+   nombrePlastico.textContent= (name);
+} )
+
 
 //console.log(validator);//
--- src/validator.js
+++ src/validator.js
@@ -1,4 +1,46 @@
 const validator = {
+isvalid: function (creditCardNumber){
+//pasar el # a un array en orden inverso
+let arrayReversed = creditCardNumber.split('').reverse();
+for (let i = 0; i < arrayReversed.length; i++) {
+  if (i % 2 != 0) {
+    arrayReversed[i] = arrayReversed[i] * 2;
+    if (arrayReversed[i] > 9) {
+      let getDigits = arrayReversed[i].toString().split('');
+      let addDigits = 0;
+      getDigits.forEach(function(element) {
+        addDigits += parseInt(element);
+      });
+      arrayReversed[i] = addDigits;
+    }
+    else {
+      arrayReversed[i] = arrayReversed[i] * 1;
+    }
+  }
+  else {
+    arrayReversed[i] = arrayReversed[i] * 1;
+  }
+}
+
+let addTotal = 0;
+arrayReversed.forEach(function(value) {
+  addTotal += value;
+});
+let residue = addTotal % 10;
+
+if (residue == 0) {
+  return true;
+}
+else {
+  return false;
+}
+},
+
+
+
+
+
+
   maskify: function (creditCardNumber) {
 
 
